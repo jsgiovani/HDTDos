a8b147fb79de6edc7e3feaa9922a56ebdad822f1
jsgiovani <josgiovani@hotmail.com>
2017-02-07 21:44:53 -0600

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,27 +1,65 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import javax.annotation.processing.Messager;
-
-public class LeerTxt {
-	String texto= "";
-	
-	public String leerTxt(String direccion){  //Direccion donde esta hubicado el archivo txt
-		try{
-			BufferedReader bf = new BufferedReader(new FileReader(direccion));
-			String temporal = "";
-			String bfReader;
-			//Se realiza el siclo mientras bfReader aun contiene datos
-			while((bfReader=bf.readLine()) != null){
-				temporal = temporal + bfReader;
-			
-			}
-			texto = temporal;			
-			
-		} catch (Exception e){
-			
-			System.err.println("No se encontro el archivo txt solicitado");
-		}
-		return texto;
-	}
+/**
+ *
+ * @author User
+ */
+public class Lista {
+    
+    //todas las listas deben tener referencias hacia el inico y al fina de la lista
+    //cola y inicio 
+    protected Nodo inicio,fin;//punteros
+    
+    //constructor
+    public Lista(){
+        //apuntan a nada 
+        inicio=null;
+        fin=null;
+    }
+    
+    
+    //Método para saber si la lista esta vacia.
+    public boolean estaVacia(){
+        if(inicio==null){
+            return true;// si esta vacia regresa un verdadero
+        }
+        else{
+            return false;
+        }
+    }
+    //Método para agrear un nodo al inicio de la lista
+    public void agregarAlInicio(String elemento){
+        //creando al nodo
+        inicio=new Nodo(elemento, inicio);
+        if(fin==null){ //si fin es igual a null significa que aún no habian nodos
+            fin=inicio;
+        }
+    }
+    //Método para insertar nodo al Final de la 
+    public void agregarAlFinal(String elemento){
+        if (!estaVacia()){//si no esta vacia ,algo tiene
+            fin.siguiente=new Nodo(elemento);
+            fin=fin.siguiente;
+        }else{//si esta vacia 
+            inicio=fin=new Nodo(elemento);
+            
+        }
+        
+    } 
+    //Método para mostrar los datos
+    public void mostrarLista(){
+        Nodo recorrer=inicio;
+        //mientras recorrer sea diferente de nullo, es decir que mientras no llgue al final 
+        while(recorrer!=null){
+            System.out.print("["+recorrer.dato+"]\n");
+            recorrer=recorrer.siguiente;
+        }
+    
+    }
+    
+    
 }
